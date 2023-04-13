29d6dcea9d02e1881ffd817d2db4a112eeced7e7
Vladimir <volodyabrus@gmail.com>
2023-04-13 22:31:54 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 5437824033 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return proportions_ztest((x_success, y_success), (x_cnt, y_cnt))[1] < 0.01
